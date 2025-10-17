f7cc77e9285e28307d7ea05c0a4a6b8fb3a25886
Siddu Deshetti <deshettisiddhu@gmail.com>
2025-10-17 19:13:02 +0530

    problem 1 added

--- problem_1.cpp
+++ problem_1.cpp
@@ -1,12 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
+void sumofmultiples(int x){
+    long long sum=0;
+    for(int i=1;i<x;i++){
+        if(i%3==0 || i%5==0){
+            sum+=i;
+        }
+    }
+    cout<<sum;
+}
 
 int main() {
-    int n;
-    cin>>n;;
-    cout<<n;
+    
+    int x;
+    cin>>x;
+    sumofmultiples(x);
     
     return 0;
 }
