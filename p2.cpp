dae980708111d8cffbbfbf3ad54af90ee3793e63
Siddu Deshetti <deshettisiddhu@gmail.com>
2025-10-17 18:43:56 +0530

    Added p2.cpp and updated problem_1.cpp

---
+++ p2.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*some observations
+
+
+*/
+
+void solve() {
+    
+    //inputs
+    
+    
+    
+    //Algorithm
+    
+    
+    
+    //Output
+    
+    
+    
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    //solve();
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
--- problem_1.cpp
+++ problem_1.cpp
@@ -4,7 +4,6 @@ using namespace std;
 
 
 int main() {
-    
     int n;
     cin>>n;;
     cout<<n;
